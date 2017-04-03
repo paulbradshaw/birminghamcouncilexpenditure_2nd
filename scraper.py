f4a1e112e483e1726fa31e0da595906593a5a758
Paul Bradshaw <paulonhismobile@gmail.com>
2017-04-03 17:32:25 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,18 +1,20 @@
 import scraperwiki
 import csv
-#for the previous installment see https://scraperwiki.com/scrapers/birminghamcouncilexpenditure/
+#guidance on csv library at https://scraperwiki.com/docs/python/python_csv_guide/
 
-#more on csv library: https://scraperwiki.com/docs/python/python_csv_guide/
-
-data = scraperwiki.scrape('http://www.birmingham.gov.uk/cs/Satellite?blobcol=urldata&blobheader=application%2Fvnd.ms-excel&blobheadername1=Content-Disposition&blobkey=id&blobtable=MungoBlobs&blobwhere=1223518937294&ssbinary=true&blobheadervalue1=attachment%3B+filename%3D300627Payments_over_%C2%A3500_October_2012.csv')
+#scrape the csv file into new variable 'data'
+data = scraperwiki.scrape('https://data.birmingham.gov.uk/dataset/14492d37-1a77-4d46-9204-27363fc62149/resource/bacf38dd-3530-4c95-a0c3-83e21c9b2259/download/sgmsreportsvcsfreportsvcsfreports201415vcsfreport2014qtr1final.csv')
 
 #first attempt generated an error: "SqliteError: Binary strings must be utf-8 encoded"
 #So we need to trace which part of our data is responsible. We're going to 
 
+#use .reader function and .splitlines() method to put 'data' into csv object 'reader'
 reader = csv.reader(data.splitlines())
+print reader
 
 record = {}
 
+idno = 0
 #Some code that decodes UTF-8
 #http://yosaito.co.uk/scraperlibs/python/scraperwiki/sqlite.py
 
@@ -25,14 +27,18 @@ for row in reader:
     print type(row[5])
     print type(row[6])
     print type(row[7])
-    record['Vendor'] = row[0].decode("utf-8")
-    record['Vendor Name'] = row[1].decode("utf-8")
-    record[' Invoice Amount '] = row[2].decode("latin-1")
-    record['Payment Date'] = row[3].decode("utf-8")
-    record['Doc Number'] = row[4].decode("utf-8")
-    record['Invoice Ref'] = row[5].decode("utf-8")
-    record['Cost Cente'] = row[6].decode("utf-8")
-    record['Directorate'] = row[7].decode("utf-8")
+    record['ref'] = row[0]
+    record['organisation'] = row[1]
+    record['status'] = row[2]
+    record['start_date'] = row[3]
+    record['end_date'] = row[4]
+    record['revised_end'] = row[5]
+    record['directorate'] = row[6]
+    record['funding'] = row[7] #.decode("latin-1")
+    idno += 1
+    record['ID'] = idno
     print record
-    scraperwiki.sqlite.save(['Invoice Ref'], record)
+    scraperwiki.sqlite.save(['ID'], record)
 
+#This will generate an error with this data:
+#SqliteError: Binary strings must be utf-8 encoded
